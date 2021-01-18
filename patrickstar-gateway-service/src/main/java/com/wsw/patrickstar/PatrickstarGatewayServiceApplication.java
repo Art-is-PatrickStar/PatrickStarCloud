55d8d986c9ca40c69b7bfaed07881c2854d04b7c
wangsongwen <2544894086wsw@gmail.com>
2021-01-18 14:29:37 +0800

    2021-01-18 update

---
+++ patrickstar-gateway-service/src/main/java/com/wsw/patrickstar/PatrickstarGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wsw.patrickstargatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrickstarGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatrickstarGatewayServiceApplication.class, args);
+    }
+
+}
