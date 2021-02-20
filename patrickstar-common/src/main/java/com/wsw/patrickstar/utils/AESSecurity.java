dfb587e22fea5a86c29d17fb9f4f89dad3c0cb4f
wangsongwen <2544894086wsw@gmail.com>
2021-02-20 15:47:38 +0800

    guava本地缓存

---
+++ patrickstar-common/src/main/java/com/wsw/patrickstar/utils/AESSecurity.java
@@ -0,0 +1,10 @@
+package com.wsw.patrickstar.utils;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 14:38 2021/2/20
+ * @Description:
+ */
+public class AESSecurity {
+
+}
---
+++ patrickstar-common/src/main/java/com/wsw/patrickstar/utils/RSASecurity.java
@@ -0,0 +1,6 @@
+package com.wsw.patrickstar.utils;/**
+* @Author WangSongWen
+* @Date: Created in 14:38 2021/2/20
+* @Description: 
+*/public class RSASecurity {
+}
