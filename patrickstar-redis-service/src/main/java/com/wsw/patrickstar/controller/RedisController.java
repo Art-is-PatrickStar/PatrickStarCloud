7435faf3676765114599548dadc1c39159a3f8ae
wangsongwen <2544894086wsw@gmail.com>
2021-08-02 15:52:35 +0800

    update redis service

--- patrickstar-redis-service/src/main/java/com/wsw/patrickstar/controller/RedisController.java
+++ patrickstar-redis-service/src/main/java/com/wsw/patrickstar/controller/RedisController.java
@@ -3,12 +3,10 @@ package com.wsw.patrickstar.controller;
 import com.wsw.patrickstar.service.RedisService;
 import com.wsw.patrickstar.util.RedisLock;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author WangSongWen
@@ -23,7 +21,30 @@ public class RedisController {
     private RedisService redisService;
 
     @GetMapping("/tryLock")
-    RedisLock tryLock(@RequestParam("lockKey") String lockKey) {
+    public RedisLock tryLock(@RequestParam("lockKey") String lockKey) {
         return redisService.tryLock(lockKey);
+    }
+
+    @PostMapping("/setRefreshToken")
+    public void setRefreshToken(@RequestParam("hashName") String hashName, @RequestParam("hashKey") String hashKey,
+                                @RequestParam("hashValue") String hashValue) {
+        redisService.setRefreshToken(hashName, hashKey, hashValue);
+    }
+
+    @PostMapping("/getRefreshToken")
+    public String getRefreshToken(@RequestParam("hashName") String hashName, @RequestParam("hashKey") String hashKey) {
+        return redisService.getRefreshToken(hashName, hashKey);
+    }
+
+    @PostMapping("/setRefreshTokenExpire")
+    public void setRefreshTokenExpire(@RequestParam("hashName") String hashName, @RequestParam("timeOut") long timeOut,
+                                      @RequestParam("unit") TimeUnit unit) {
+        redisService.setRefreshTokenExpire(hashName, timeOut, unit);
+    }
+
+    @PostMapping("/setOldToken")
+    void setOldToken(@RequestParam("key") String key, @RequestParam("value") String value, @RequestParam("timeOut") long timeOut,
+                     @RequestParam("unit") TimeUnit unit) {
+        redisService.setOldToken(key, value, timeOut, unit);
     }
 }
--- patrickstar-redis-service/src/main/java/com/wsw/patrickstar/service/impl/RedisServiceImpl.java
+++ patrickstar-redis-service/src/main/java/com/wsw/patrickstar/service/impl/RedisServiceImpl.java
@@ -4,9 +4,11 @@ import com.wsw.patrickstar.Constants.RedisConstants;
 import com.wsw.patrickstar.service.RedisService;
 import com.wsw.patrickstar.util.RedisDistributedLock;
 import com.wsw.patrickstar.util.RedisLock;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author WangSongWen
@@ -16,11 +18,34 @@ import javax.annotation.Resource;
 @Service
 public class RedisServiceImpl implements RedisService {
     @Resource
+    private StringRedisTemplate redisTemplate;
+
+    @Resource
     private RedisDistributedLock redisDistributedLock;
 
     @Override
     public RedisLock tryLock(String lockKey) {
         return redisDistributedLock.tryLock(RedisConstants.REDIS_LOCK_PREFIX + lockKey,
                 RedisConstants.REDIS_LOCK_WAIT_TIME, RedisConstants.REDIS_LOCK_LEASE_TIME, RedisConstants.REDIS_LOCK_TIME_UNIT);
+    }
+
+    @Override
+    public void setRefreshToken(String hashName, String hashKey, String hashValue) {
+        redisTemplate.opsForHash().put(hashName, hashKey, hashKey);
+    }
+
+    @Override
+    public String getRefreshToken(String hashName, String hashKey) {
+        return (String) redisTemplate.opsForHash().get(hashName, hashKey);
+    }
+
+    @Override
+    public void setRefreshTokenExpire(String hashName, long timeOut, TimeUnit unit) {
+        redisTemplate.expire(hashName, timeOut, unit);
+    }
+
+    @Override
+    public void setOldToken(String key, String value, long timeOut, TimeUnit unit) {
+        redisTemplate.opsForValue().set(key, value, timeOut, unit);
     }
 }
