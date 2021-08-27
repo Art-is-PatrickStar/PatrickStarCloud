789a2812cabe0c1d454967e6bc8c1b7b621a3a9c
wangsongwen <2544894086wsw@gmail.com>
2021-08-27 16:46:40 +0800

    分库分表查询
    分表规则对于 >= <= 不生效 对于 between and 生效 why? 可能自定义分片算法有问题

--- patrickstar-task-service/src/main/java/com/wsw/patrickstar/mapper/TaskMapper.java
+++ patrickstar-task-service/src/main/java/com/wsw/patrickstar/mapper/TaskMapper.java
@@ -3,6 +3,10 @@ package com.wsw.patrickstar.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wsw.patrickstar.entity.Task;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @Author WangSongWen
@@ -11,5 +15,15 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface TaskMapper extends BaseMapper<Task> {
+    /**
+     * 分表查询测试
+     * @param startDate
+     * @param endDate
+     * @return
+     */
+    List<Task> selectTaskByTime1(@Param("startDate") String startDate, @Param("endDate") String endDate);
 
+    List<Task> selectTaskByTime2(@Param("date") Date date);
+
+    List<Task> selectTaskByTime3(List<Date> timeList);
 }
