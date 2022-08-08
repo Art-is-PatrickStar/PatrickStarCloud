8b6ce376c9ab311a4cb13f7253428be02127e37d
wangsongwen <2544894086wsw@gmail.com>
2022-08-08 18:01:16 +0800

    update

---
+++ patrickstar-common/src/main/java/com/wsw/patrickstar/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.wsw.patrickstar.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: wangsongwen
+ * @Date: 2022/8/8 17:07
+ * Copyright © 2022 Hundsun Technologies Inc. All Rights Reserved
+ */
+@Slf4j
+@RestController
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
