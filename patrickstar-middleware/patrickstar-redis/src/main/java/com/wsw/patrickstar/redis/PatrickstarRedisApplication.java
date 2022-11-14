a9e77e2c772ec3c8a997a204227bfc37a6cb4aed
wangsongwen <2544894086wsw@gmail.com>
2022-11-15 00:12:45 +0800

    add independent redis service

---
+++ patrickstar-middleware/patrickstar-redis/src/main/java/com/wsw/patrickstar/redis/PatrickstarRedisApplication.java
@@ -0,0 +1,13 @@
+package com.wsw.patrickstar.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrickstarRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatrickstarRedisApplication.class, args);
+    }
+
+}
